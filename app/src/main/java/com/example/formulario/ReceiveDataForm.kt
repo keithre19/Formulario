516151157df6688bef5337341e0caaefe59bd6e0
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-07-12 20:38:59 -0600

    add receive data and modify design

--- app/src/main/java/com/example/formulario/ReceiveDataForm.kt
+++ app/src/main/java/com/example/formulario/ReceiveDataForm.kt
@@ -1,6 +1,7 @@
 package com.example.formulario
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,22 @@ class ReceiveDataForm : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var TextViewName: TextView = findViewById(R.id.textViewName)
+        var TextViewLastName: TextView = findViewById(R.id.textViewLastName)
+        var TextViewPhone: TextView = findViewById(R.id.textViewPhoneNumber)
+        var TextViewAge: TextView = findViewById(R.id.textViewAge)
+        var TextViewEmail: TextView = findViewById(R.id.textViewEmail)
+
+        val name = intent.getStringExtra("name") ?: "No se recibió el nombre de la persona"
+        val lastName = intent.getStringExtra("lastName") ?: "No se recibió el apellido"
+        val phone = intent.getStringExtra("phone") ?: "No se recibió el teléfono"
+        val age = intent.getStringExtra("age") ?: "No se recibió la edad"
+        val email = intent.getStringExtra("email") ?: "No se recibió el correo"
+
+        TextViewName.text = name
+        TextViewLastName.text = lastName
+        TextViewPhone.text = phone
+        TextViewAge.text = age
+        TextViewEmail.text = email
     }
 }
