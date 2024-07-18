b781fd3a86e7a5a5653222c11ffbc26a907eaa21
Keithleen Ivana Ruíz Estrada <125908174+keithre19@users.noreply.github.com>
2024-07-18 17:06:06 -0600

    design of the form and data submission to the second activity

--- app/src/main/java/com/example/formulario/MainActivity.kt
+++ app/src/main/java/com/example/formulario/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.formulario
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val nombre = findViewById<EditText>(R.id.txtNombre)
+        val apellido = findViewById<EditText>(R.id.txtApellido)
+        val telefono = findViewById<EditText>(R.id.txtTelefono)
+        val edad = findViewById<EditText>(R.id.txtEdad)
+        val correo = findViewById<EditText>(R.id.txtCorreo)
+
+        val enviarDatos = findViewById<Button>(R.id.btnIngresar)
+        enviarDatos.setOnClickListener{
+            if(nombre.text.toString().isEmpty()){
+                nombre.error = "Debe llenar este campo"
+            } else if(apellido.text.toString().isEmpty()){
+                apellido.error = "Debe llenar este campo"
+            } else if(telefono.text.toString().isEmpty()){
+                telefono.error = "Debe llenar este campo"
+            } else if(edad.text.toString().isEmpty()){
+                edad.error = "Debe llenar este campo"
+            } else if(correo.text.toString().isEmpty()){
+                correo.error = "Debe llenar este campo"
+            } else {
+                val enviarDatosIntent = Intent(this, ReceiveDataForm::class.java).apply{
+                    putExtra("nombre", nombre.text.toString())
+                    putExtra("apellido", apellido.text.toString())
+                    putExtra("telefono", telefono.text.toString())
+                    putExtra("edad", edad.text.toString())
+                    putExtra("correo", correo.text.toString())
+                }
+                startActivity(enviarDatosIntent)
+            }
+        }
+        
     }
 }
